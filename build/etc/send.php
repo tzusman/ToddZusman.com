168ef86bec7af5802b739dcf67899d0a95b567a8
Todd Zusman <toddzusman@86.12.224.10.in-addr.arpa>
2010-04-05 20:29:22 -0400

    changes

--- build/etc/send.php
+++ build/etc/send.php
@@ -5,10 +5,13 @@
 		$to = 'toddzusman@gmail.com';
 		$subject = 'ToddZusman.com Contact';
 	
-		$msg = sprintf( 'Sent on %s', date('F j, Y \a\t g:ia') );
+		$msg  = sprintf( 'Sent on %s', date('F j, Y \a\t g:ia') );
 		$msg .= sprintf( "\nName: %s", $_POST['name'] );
 		$msg .= sprintf( "\nEmail: %s", $_POST['email'] );
 		$msg .= sprintf( "\n\n%s", $_POST['about'] );
+
+		$hdrs  = "From: ToddZusman.com\r\n";
+    $hdrs .= sprintf( "Reply-To: %s\r\n", $_POST['email'] );
 
 		mail( $to, $subject, $msg );	
 	
